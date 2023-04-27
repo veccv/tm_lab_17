53d3716a5da9a6d6e43ce4e1f08b20daa19a2362
jacek <dd@dd.pl>
2023-04-27 18:40:46 +0200

    Punkt 8

--- index1.php
+++ index1.php
@@ -1,13 +1,19 @@
-<body> 
+<body>
 <?php
 include 'Database.php';
 print "<TABLE CELLPADDING=5 BORDER=1>";
-print "<TR><TD>id</TD><TD>Host</TD><TD>Port</TD></TR>\n";
+print "<TR><TD>id</TD><TD>Host</TD><TD>Port</TD><TD>Stan</TD></TR>\n";
 foreach (mysqli_fetch_all(Database::getConnection()->query("SELECT * FROM domeny")) as $wiersz) {
     $id = $wiersz[0];
     $host = $wiersz[1];
     $port = $wiersz[2];
-    print "<TR><TD>$id</TD><TD>$host</TD><TD>$port</TD></TR>\n";
+    $fp = @fsockopen($host, $port, $errno, $errstr, 30);
+    if ($fp) {
+        $stan = "Ok";
+    } else {
+        $stan = "$errno $errstr";
+    }
+    print "<TR><TD>$id</TD><TD>$host</TD><TD>$port</TD><TD>$stan</TD></TR>\n";
 }
 print "</TABLE>";
 Database::getConnection()->close();
