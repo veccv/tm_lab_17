c60df56d8a50af7c924f4cebf38e3b69c73cb9c3
jacek <dd@dd.pl>
2023-04-30 12:24:06 +0200

    Added admin

--- index1.php
+++ index1.php
@@ -98,18 +98,34 @@ $session = $_SESSION['loggedin'];
             print "<TABLE CELLPADDING=5 BORDER=1>";
             print "<TR><TD>id</TD><TD>Host</TD><TD>Port</TD><TD>Stan</TD></TR>\n";
             $user_id = $_SESSION['user_id'];
-            foreach (mysqli_fetch_all(Database::getConnection()->query("SELECT * FROM domeny WHERE user_id=$user_id")) as $wiersz) {
-                $id = $wiersz[0];
-                $host = $wiersz[1];
-                $port = $wiersz[2];
-                $stan = ping($host, 80, 1);
-                if (strlen((string)$stan) > 0) {
-                    $stan = "Online";
-                } else {
-                    $stan = "Offline";
-                }
+            if ($_SESSION["login"] == "admin") {
+                foreach (mysqli_fetch_all(Database::getConnection()->query("SELECT * FROM domeny")) as $wiersz) {
+                    $id = $wiersz[0];
+                    $host = $wiersz[1];
+                    $port = $wiersz[2];
+                    $stan = ping($host, 80, 1);
+                    if (strlen((string)$stan) > 0) {
+                        $stan = "Online";
+                    } else {
+                        $stan = "Offline";
+                    }
 
-                print "<TR><TD>$id</TD><TD>$host</TD><TD>$port</TD><TD>$stan</TD></TR>\n";
+                    print "<TR><TD>$id</TD><TD>$host</TD><TD>$port</TD><TD>$stan</TD></TR>\n";
+                }
+            } else {
+                foreach (mysqli_fetch_all(Database::getConnection()->query("SELECT * FROM domeny WHERE user_id=$user_id")) as $wiersz) {
+                    $id = $wiersz[0];
+                    $host = $wiersz[1];
+                    $port = $wiersz[2];
+                    $stan = ping($host, 80, 1);
+                    if (strlen((string)$stan) > 0) {
+                        $stan = "Online";
+                    } else {
+                        $stan = "Offline";
+                    }
+
+                    print "<TR><TD>$id</TD><TD>$host</TD><TD>$port</TD><TD>$stan</TD></TR>\n";
+                }
             }
             print "</TABLE>";
             Database::getConnection()->close();
